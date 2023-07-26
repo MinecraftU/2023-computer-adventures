ead13114f9f7d3f153bcc845f8f71e2b0e33d3b7
James Li <jamespeilunli@gmail.com>
2023-07-26 12:29:44 -0700

    allow user to move pieces; print moves to console; add make_move fn to engine

--- client/js/index.js
+++ client/js/index.js
@@ -1,2 +1,22 @@
+/*
 var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
 var board = Chessboard('myBoard')
+*/
+function onDrop (source, target, piece, newPos, oldPos, orientation) {
+  console.log('Source: ' + source);
+  console.log('Target: ' + target);
+  //console.log('Piece: ' + piece);
+  //console.log('New position: ' + Chessboard.objToFen(newPos));
+  //console.log('Old position: ' + Chessboard.objToFen(oldPos));
+  //console.log('Orientation: ' + orientation);
+  //console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDrop: onDrop,
+  sparePieces: true
+}
+var board = Chessboard('myBoard', config);
+
