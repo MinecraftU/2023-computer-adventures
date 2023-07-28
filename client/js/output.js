f7d4f4f957beb90df8c255b0142f0c39b33b2bf0
James Li <jamespeilunli@gmail.com>
2023-07-28 09:56:07 -0700

    persist rust board across moves

--- client/js/output.js
+++ client/js/output.js
@@ -3,7 +3,6 @@ export function my_alert(message) {
     console.log(message);
 }
 
-
 export function update_board(fen) {
     console.log("JS: updating board to ", fen);
     window.board.position(fen.split(" ")[0]);
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,14 @@
 use chess::{Board, ChessMove, Square, MoveGen};
 use wasm_bindgen::prelude::*;
 use std::str::FromStr;
+use std::sync::Mutex;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref BOARD : Mutex<Board> = Mutex::new(Board::default());
+}
 
 #[wasm_bindgen(module="/client/js/output.js")]
 extern {
@@ -19,10 +27,10 @@ pub fn get_engine_move(source_str: &str, target_str: &str) { // TODO: recieves a
     let source : Square = Square::from_str(source_str).unwrap();
     let target : Square = Square::from_str(target_str).unwrap();
 
-    let board = Board::default();
-    let result = make_move(source, target, &board);
+    let result = make_move(source, target, &BOARD.lock().unwrap());
     let engine_move = evaluate(&result).unwrap();
     let result2 = make_move(engine_move.get_source(), engine_move.get_dest(), &result);
+    *BOARD.lock().unwrap() = result2;
     update_board(&result2.to_string());
 }
 
@@ -39,26 +47,3 @@ fn make_move(from : Square, to : Square, board : &Board) -> Board {
     board.make_move(chess_move, &mut result);
     return result;
 }
-/*
-fn main() {
-    /*
-    let fen = "rnbqkbnr/ppp1pppp/8/3p4/3P4/5N2/PPP1PPPP/RNBQKB1R w KQkq - 0 1";
-    let board = Board::from_str(fen);
-    println!("{}", board.expect("REASON").king_square(Color::White));
-    //assert_eq!(board.expect("REASON").king_square(Color::White), Square::E1);
-    */
-
-    /*
-    let board = Board::default();
-    let mut result = Board::default();
-    board.make_move(ChessMove::new(Square::D2, Square::D4, None), &mut result);
-
-    let mut result2 = result; 
-    result.make_move(ChessMove::new(Square::E2, Square::E4, None), &mut result2);
-    //assert_eq!(result.side_to_move(), Color::Black);
-    */
-    let board = Board::default();
-    act_on_board(&make_move(Square::D7, Square::D5, &make_move(Square::E2, Square::E8, &board)));
-
-}
-*/
