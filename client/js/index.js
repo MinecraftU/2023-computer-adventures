9dd532ed55ec0fc487fea4789f78e24ccb72fd6d
James Li <jamespeilunli@gmail.com>
2023-07-31 10:57:19 -0700

    clean up code

--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,17 @@
 import init, { get_engine_move } from "../pkg/chess_engine.js";
 
 function wait_until(source, target, oldPos) { // https://stackoverflow.com/a/64947598
-    if (window.promotion === undefined) {
+    if (!window.promotion) {
         window.setTimeout(wait_until, 100, source, target, oldPos);
     } else {
         console.log("JS: done waiting for player to choose promotion piece");
         make_move(source, target, oldPos);
-        window.promotion = undefined;
+        window.promotion = null;
     }
 }
 
 function make_move(source, target, oldPos) {
-    let promotion = window.promotion === undefined ? "" : window.promotion;
-    let new_position = get_engine_move(source, target, promotion);;
+    let new_position = get_engine_move(source, target, window.promotion || "");;
     console.log("js: returned engine move:", new_position);
     if (new_position === "illegal move") {
         window.board.position(oldPos);
@@ -26,8 +25,7 @@ function onDrop(source, target, piece, newPos, oldPos, orientation) {
 
     init().then(() => {
         if (target[1] === "8" && piece === "wP") { // player must promote
-            let promotion_choice = document.getElementById("promotion-choice");
-            promotion_choice.style.display = "block";
+            document.getElementById("promotion-choice").style.display = "block";
             wait_until(source, target, oldPos);
             return;
         }
--- client/js/promotion.js
+++ client/js/promotion.js
@@ -2,6 +2,5 @@ function promote_to(piece) {
     console.log("JS: promoting to", piece);
     window.promotion = piece;
 
-    let promotion_choice = document.getElementById("promotion-choice");
-    promotion_choice.style.display = "none";
+    document.getElementById("promotion-choice").style.display = "none";
 }
