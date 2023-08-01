7f81c8d91b9ae20b1e4f715891d1af9a65ee14d5
James Li <jamespeilunli@gmail.com>
2023-08-01 12:06:59 -0700

    add alpha-beta pruning

--- src/search.rs
+++ src/search.rs
@@ -8,6 +8,42 @@ extern {
     pub fn my_alert(s: &str);
 }
 
+pub fn alpha_beta_max(mut alpha : f32, mut beta : f32, depth : i32, board : &Board) -> f32 { // black, engine
+    if depth == 0 {
+        return evaluate::evaluate(board);
+    };
+    for chess_move in MoveGen::new_legal(&board) {
+        let mut result = *board;
+        board.make_move(chess_move, &mut result);
+        let score: f32 = alpha_beta_min(alpha, beta, depth - 1, &result);
+        if score >= beta {
+            return beta;
+        }
+        if score > alpha {
+            alpha = score;
+        }
+    }
+    return alpha;
+}
+
+pub fn alpha_beta_min(mut alpha : f32, mut beta : f32, depth : i32, board : &Board) -> f32 {
+    if depth == 0 {
+        return -evaluate::evaluate(board);
+    };
+    for chess_move in MoveGen::new_legal(&board) {
+        let mut result = *board;
+        board.make_move(chess_move, &mut result);
+        let score: f32 = alpha_beta_max(alpha, beta, depth - 1, &result);
+        if score <= alpha {
+            return alpha;
+        }
+        if score < beta {
+            beta = score;
+        }
+    }
+    return beta;
+}
+
 pub fn maxi(depth : i32, board : &Board) -> f32 { // black, engine
     if depth == 0 {
         return evaluate::evaluate(board);
@@ -46,7 +82,7 @@ pub fn search(board : &Board) -> Option<ChessMove> {
     for chess_move in MoveGen::new_legal(&board) {
         let mut result = *board;
         board.make_move(chess_move, &mut result);
-        let score: f32 = mini(2, &result);
+        let score: f32 = alpha_beta_min(f32::MIN, f32::MAX, 3, &result);
         if score > max {
             max = score;
             best_move = Some(chess_move);
