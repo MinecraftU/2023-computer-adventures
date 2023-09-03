f07061ede10b673217101dd0eb2e65d874d4ed74
James Li <jamespeilunli@gmail.com>
2023-09-03 15:04:14 -0700

    clean up

--- src/search.rs
+++ src/search.rs
@@ -44,38 +44,6 @@ pub fn alpha_beta_min(alpha : f32, mut beta : f32, depth : i32, board : &Board) 
     return beta;
 }
 
-pub fn maxi(depth : i32, board : &Board) -> f32 { // black, engine
-    if depth == 0 {
-        return evaluate::evaluate(board);
-    };
-    let mut max: f32 = f32::MIN;
-    for chess_move in MoveGen::new_legal(&board) {
-        let mut result = *board;
-        board.make_move(chess_move, &mut result);
-        let score: f32 = mini(depth - 1, &result);
-        if score > max {
-            max = score;
-        }
-    }
-    return max;
-}
-
-pub fn mini(depth : i32, board : &Board) -> f32 {
-    if depth == 0 {
-        return evaluate::evaluate(board);
-    };
-    let mut min: f32 = f32::MAX;
-    for chess_move in MoveGen::new_legal(&board) {
-        let mut result = *board;
-        board.make_move(chess_move, &mut result);
-        let score: f32 = maxi(depth - 1, &result);
-        if score < min {
-            min = score;
-        }
-    }
-    return min;
-}
-
 pub fn search(board : &Board) -> Option<ChessMove> {
     let mut best_move: Option<ChessMove> = None;
     let mut max: f32 = f32::MIN;
@@ -115,4 +83,3 @@ mod tests {
         assert_ne!(search(&result), None);
     }
 }
-
